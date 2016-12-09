9b71de7fbc8d80cdd562dc003df67095f274f85f
tangzanarak5 <nonstop_tang@hotmail.com>
2016-12-09 13:31:32 +0700

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.get('/webhook', function(req, res) {
-  var key = 'EAAZAdh4yZAgXcBAMdX9U6kUTd2Ow5oWyg6QMVy95UXvbnfALZCvovTiflTqHGhMChSqzdLyGWgIg1Sivp4dW8H5my5EIxt4TEZB3hchmNcDOkZBZBZC6aan9IVFhiVlYag6wRB6ZBsHlEFCpZAdAnCUSvLrXvF16AZC6U0Pf9pyqneuwZDZD'
+  var key = 'EAADdgeiXCmIBANtmNzvdmuMkoqmisxvcNLbclSnGPUAZBMf2saZBwj0ndtrDDAbkDB9vC3mMZBENUZAWkssC3JJ2Qy9oHkPL1kqtUwL1dUa4kLOuzu1JothmrHadTgrXUZAFhgt6KK02cZCLQqN84z3Wl5DzGEUT4PfWbgwGZCA5gZDZD'
   if (req.query['hub.verify_token'] === key) {
     res.send(req.query['hub.challenge'])
   }
@@ -100,7 +100,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: 'EAAZAdh4yZAgXcBABVjIsKHFWDvLmeuARXQYDYIuonG4MUnRi2DLZBsVuJZAnHhEo5I0XjSmb9Qc7x2ZAmK9JqCksGRI7hal2B4sufmaQjcxoyiXAmKZCzMqdfGofCQccMMIyTpUAimRbOsEQagekugUJx3aFadcKneB7fT74uZCaQZDZD' },
+    qs: { access_token: 'EAADdgeiXCmIBANtmNzvdmuMkoqmisxvcNLbclSnGPUAZBMf2saZBwj0ndtrDDAbkDB9vC3mMZBENUZAWkssC3JJ2Qy9oHkPL1kqtUwL1dUa4kLOuzu1JothmrHadTgrXUZAFhgt6KK02cZCLQqN84z3Wl5DzGEUT4PfWbgwGZCA5gZDZD' },
     method: 'POST',
     json: messageData
 
